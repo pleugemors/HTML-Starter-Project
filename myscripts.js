31c792dbc07480b685d9e8f724d8192a6c6e7e17
Paul Leugemors <pleugemors@yahoo.com>
2021-06-25 03:14:40 -0500

    removed some console.log

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,8 @@
 	const url = new URL(window.location.href);
-	console.log(url.search);
 	var name = url.searchParams.get('character_name');
 	var race = url.searchParams.get('Race');
 	var sex = url.searchParams.get('sex');
 	var realm = url.searchParams.get('realm');
-	console.log(name);
-	console.log(race);
-	console.log(sex);
-	console.log(realm);
 	document.getElementById("name").innerHTML = "The Name of your character is: " + name;
 	document.getElementById("race").innerHTML = "The Race of your character is: " + race;
 	document.getElementById("realm").innerHTML = "The Realm you will start in is: " + realm;
